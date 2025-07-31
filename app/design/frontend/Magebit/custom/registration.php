a67164ca65d86a2f2ef3551a11db10af15ed3f12
janiscript <janis.berzins@magebit.com>
2025-07-31 15:57:56 +0300

    Fix Related Products layout and styling

---
+++ app/design/frontend/Magebit/custom/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © Magebit, LLC. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Magebit/custom',
+    __DIR__
+); 

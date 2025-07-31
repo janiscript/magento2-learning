7540078bdfab574ebfd07d6d76a0dfd3883eeb26
janiscript <janis.berzins@magebit.com>
2025-07-31 14:30:23 +0300

    Clean Magento 2 installation - removed old custom modules and themes

---
+++ app/etc/env.php
@@ -0,0 +1,92 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_e8uynwi'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'ychiBGDG0rbJwJtBDoSZ5B38Qn8gHcr1'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'e09_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'e09_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'config' => [
+        'async' => 0
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'base64KoRcyvIP1ywCPYyqMi3xSsMYufh3kLXftMmG6OeMm+I='
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => '127.0.0.1',
+                'dbname' => 'magentojanis',
+                'username' => 'root',
+                'password' => 'option123',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'graphql_query_resolver_result' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'magentojanis.local'
+    ],
+    'install' => [
+        'date' => 'Wed, 30 Jul 2025 06:10:48 +0000'
+    ],
+    'db_logger' => [
+        'output' => 'disabled'
+    ]
+];
